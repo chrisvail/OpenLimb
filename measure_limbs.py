ace2913ad43d0c36419796170a8e8efccf8a0a21
chrisvail <christopher.n.vail@gmail.com>
2025-07-07 09:58:13 +0100

    Measurements are definitely working, questionably diffable

--- measure_limbs.py
+++ measure_limbs.py
@@ -1,12 +1,25 @@
 import torch
 
 
+def plane_edge_intersection(a, b, n, p):
+    if len(a.shape) == 2:
+        t = torch.einsum("ij, ij -> i", p - a, n) / torch.einsum("ij, ij -> i", b - a, n)
+        t = t.unsqueeze(-1)
+    else:
+        t = torch.dot(p - a, n) / torch.dot(b - a, n)
+    return torch.nan_to_num(a + t * (b - a), nan=float('inf')), t
+
+
+
+
 def measure_plane(verts, edge2vert, face2edge, plane_point, plane_normal):
     edge_verts = verts[edge2vert]
     intersections, t = plane_edge_intersection(
         edge_verts[:, 0], edge_verts[:, 1], plane_normal, plane_point
     )
+
     t_mask = torch.logical_and(0 <= t, t < 1)
+    print(f"{torch.sum(torch.isnan(intersections))=}    {torch.sum(t_mask)=}    {t_mask.shape=}")
 
     face_intersections = intersections[face2edge]
     face_distances = torch.linalg.norm(
@@ -32,5 +45,9 @@ def measure_plane(verts, edge2vert, face2edge, plane_point, plane_normal):
         dim=1,
     )
 
+    print(f"{t_mask_faces.shape=}    {torch.sum(t_mask_faces)}")
+
     return torch.sum(face_distances * t_mask_faces)
+
+
 
